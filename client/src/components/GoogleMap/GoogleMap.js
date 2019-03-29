d4142e1375e1587db2eb189eb733e0869826ce0c
Lia <liaswapna@gmail.com>
2019-03-28 18:50:05 -0700

    added intial components

---
+++ client/src/components/GoogleMap/GoogleMap.js
@@ -0,0 +1,12 @@
+import React from "react";
+class GoogleMap extends React.Component{
+    render(){
+        return(
+            <div>
+                <p>Show GoogleMap Here</p>
+            </div>
+        )
+    }
+}
+
+export default GoogleMap;
---
+++ client/src/components/Nav/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+class Nav extends React.Component{
+    render(){
+        return(
+            <div>
+                <p>Find the city in Google Maps</p>
+            </div>
+        )
+    }
+}
+
+export default Nav;
